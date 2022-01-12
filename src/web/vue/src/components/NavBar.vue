63854e90a7117772045de4f5824c6c339ec7cfb7
peppe0203 <1826689collura@zuyd.nl>
2022-01-12 11:04:31 +0100

    Made tailwind.config file smaller
    Co-Authored-By: Cédric <59084642+CedricCortenraede@users.noreply.github.com>

--- src/web/vue/src/components/NavBar.vue
+++ src/web/vue/src/components/NavBar.vue
@@ -1,7 +1,6 @@
 <template>
   <div
-    class="absolute shadow-md group w-full bottom-0 lg:bottom-10 bg-green-500 flex items-center justify-center px-5 py-5 text-white"
-  >
+    class="absolute shadow-md group w-full bottom-0 lg:bottom-10 bg-green-500 flex items-center justify-center px-5 py-5 text-white">
     <a>test</a>
   </div>
 </template>
