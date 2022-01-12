8ac19d9055ca34a1630744d6b102b806af0fef8f
peppe0203 <1826689collura@zuyd.nl>
2022-01-12 10:49:29 +0100

    Adding tailwind part 1

--- src/web/vue/src/App.vue
+++ src/web/vue/src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
+    <!--- The menu on the left --->
+    <div>
+
+    </div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Vue is working on the server!" />
+    <NavigationBar></NavigationBar>
   </div>
 </template>
 
 <script>
+// ToDo: get rid of
+// Test imports
 import HelloWorld from "./components/HelloWorld.vue";
+
+// All imports
+import NavigationBar from "./components/NavBar.vue";
+
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    NavigationBar,
+    HelloWorld
   },
 };
 </script>
---
+++ src/web/vue/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="absolute
+          shadow-md
+          group
+          w-full
+          bottom-0
+          lg:bottom-10
+          bg-green-500
+          flex
+          items-center
+          justify-center
+          px-5
+          py-5
+          text-white">
+    <a>test</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
