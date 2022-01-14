eb3174b216c1293aafb13dafc56578c2861d2ab0
Cédric Cortenraede <c.cortenraede@outlook.com>
2022-01-14 17:22:12 +0100

    Removed old helper package

--- src/test_load_model.py
+++
@@ -1,9 +0,0 @@
-from helper.utils import *
-from helper.model.evaluate import *
-from helper.model.creation.preprocess import *
-from helper.model.creation.train import *
-
-
-if __name__ == "__main__":
-    models = {}
-    print(load_model("cooler_condition", "../models/"))
