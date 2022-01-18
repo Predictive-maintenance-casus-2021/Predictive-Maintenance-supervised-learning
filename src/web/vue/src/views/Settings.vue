659d1534ea432eb521e32738e13cf58b3103bdca
peppe0203 <1826689collura@zuyd.nl>
2022-01-18 14:47:26 +0100

    Settings page made
    Co-Authored-By: Cédric <59084642+CedricCortenraede@users.noreply.github.com>

--- src/web/vue/src/views/Settings.vue
+++ src/web/vue/src/views/Settings.vue
@@ -1,19 +1,48 @@
 <template>
   <div>
-    <h1 class="text-4xl text-gray-900 font-bold">Settings</h1>
-    <div class="flex justify-center">
-      <div
-        class="border border-1 shadow-xl rounded mt-8 w-5/6 h-2/3 absolute"
-      >
-        <div class="mt-4 ml-4">
-          <a class="flex justify-center text-2xl font-medium"
-            >Algemene informatie</a
-          >
-          <a class="flex justify-center text-lg"
-            >Inzien / inladen / ontwerpen modellen</a
-          >
-        </div>
+    <h1 class="text-4xl text-gray-900 font-bold mb-8">Settings</h1>
+
+    <div class="max-w-4xl">
+      <div class="flex mb-8">
+        <button class="bg-green-400 rounded shadow px-4 py-1 text-white hover:bg-green-500 mr-4">
+          Import
+        </button>
+        <button class="bg-green-400 rounded shadow px-4 py-1 text-white hover:bg-green-500">
+          Export
+        </button>
       </div>
+
+      <form>
+      <!--
+      Minuten in de toekomst voorspellen
+      Lookback
+      Early stopping
+      Epochs
+      Shift
+      -->
+        <div class="flex items-center mb-2">
+          <label for="lookback" class="w-1/4 mr-4 text-lg font-medium text-gray-900">Lookback</label>
+          <input type="number" id="lookback" name="lookback" class="border border-1 border-gray-500 rounded w-full px-3 py-1">
+        </div>
+        <div class="flex mb-2">
+          <label for="future" class="w-1/4 mr-4 text-lg font-medium text-gray-900">Future</label>
+          <input type="number" id="future" name="future" class="border border-1 border-gray-500 rounded w-full px-3 py-1">
+        </div>
+        <div class="flex mb-2">
+          <label for="earlyStopping" class="mr-20 text-lg font-medium text-gray-900">EarlyStopping</label>
+          <input type="checkbox" id="earlyStopping" name="earlyStopping" v-model="earlyStopping" class="border border-1 border-gray-500 rounded mt-2 mr-4" style="transform: scale(1.5)">
+          <label for="earlyStopping" class="text-lg"> {{earlyStopping}}</label>
+        </div>
+        <div class="flex mb-2">
+          <label for="epochs" class="w-1/4 mr-4 text-lg font-medium text-gray-900">Epochs</label>
+          <input type="number" id="epochs" name="epochs" class="border border-1 border-gray-500 rounded w-full px-3 py-1">
+        </div>
+        <div class="flex mb-2">
+          <label for="shift" class="w-1/4 mr-4 text-lg font-medium text-gray-900">Shift</label>
+          <input type="number" id="shift" name="shift" class="border border-1 border-gray-500 rounded w-full px-3 py-1">
+        </div>
+        <button type="submit" class="text-lg bg-green-400 rounded shadow px-4 py-1 text-white hover:bg-green-500 mt-4">Save</button>
+      </form>
     </div>
   </div>
 </template>
@@ -33,4 +62,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+button:hover {
+  transform: scale(1.05);
+}
+</style>
